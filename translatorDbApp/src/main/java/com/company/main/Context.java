f03784fa8ada23724cc9e2b3da2e4f9f24f900f0
Shirali <shiralialihummatov@gmail.com>
2022-06-16 16:58:26 +0400

    DbApp

--- translatorDbApp/src/main/java/com/company/main/Context.java
+++ translatorDbApp/src/main/java/com/company/main/Context.java
@@ -1,10 +1,25 @@
 package com.company.main;
 
+import com.company.dao.impl.AzeDaoImpl;
+import com.company.dao.impl.AzeEngDaoImpl;
+import com.company.dao.impl.EngDaoImpl;
 import com.company.dao.impl.WordDaoImpl;
+import com.company.dao.inter.AzeDaoInter;
+import com.company.dao.inter.AzeEngDaoInter;
+import com.company.dao.inter.EngDaoInter;
 import com.company.dao.inter.WordDaoInter;
 
 public class Context {
     public static WordDaoInter instanceWordDao(){
         return new WordDaoImpl();
+    }
+    public static AzeDaoInter instanceAzeDao(){
+        return new AzeDaoImpl();
+    }
+    public static EngDaoInter instanceEngDao(){
+        return new EngDaoImpl();
+    }
+    public static AzeEngDaoInter instanceAzeEngDao(){
+        return new AzeEngDaoImpl();
     }
 }
