b11badc2e7c832a349ae6ababad3b8ccc8b051df
Shirali <shiralialihummatov@gmail.com>
2022-06-19 10:17:57 +0400

    DesktopApp

--- translatorDesktopApp/src/main/java/com/company/panel/AzeToEngPanel.java
+++ translatorDesktopApp/src/main/java/com/company/panel/AzeToEngPanel.java
@@ -17,6 +17,10 @@ public class AzeToEngPanel extends javax.swing.JPanel {
         initComponents();
     }
 
+    public void correctAnswer() {
+//        lblCorrect.
+    }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -26,30 +30,64 @@ public class AzeToEngPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
+        lblAze = new javax.swing.JLabel();
+        lblEng = new javax.swing.JLabel();
+        txtAze = new javax.swing.JTextField();
+        txtEng = new javax.swing.JTextField();
+        btnTranslate = new javax.swing.JButton();
 
-        jLabel1.setText("azetoeng");
+        lblAze.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
+        lblAze.setText("Azerbaijan:");
+
+        lblEng.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
+        lblEng.setText("English:");
+
+        txtAze.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+        txtAze.setText("enter the word");
+
+        txtEng.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+
+        btnTranslate.setText("translate");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(266, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addGap(239, 239, 239))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(54, 54, 54)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lblAze, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblEng))
+                .addGap(81, 81, 81)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(txtEng)
+                    .addComponent(txtAze))
+                .addGap(57, 57, 57)
+                .addComponent(btnTranslate)
+                .addContainerGap(80, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(169, 169, 169)
-                .addComponent(jLabel1)
-                .addContainerGap(218, Short.MAX_VALUE))
+                .addGap(46, 46, 46)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblAze)
+                    .addComponent(txtAze, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(68, 68, 68)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblEng)
+                    .addComponent(txtEng, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnTranslate))
+                .addContainerGap(233, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton btnTranslate;
+    private javax.swing.JLabel lblAze;
+    private javax.swing.JLabel lblEng;
+    private javax.swing.JTextField txtAze;
+    private javax.swing.JTextField txtEng;
     // End of variables declaration//GEN-END:variables
 }
--- translatorDesktopApp/src/main/java/com/company/panel/EngToAzePanel.java
+++ translatorDesktopApp/src/main/java/com/company/panel/EngToAzePanel.java
@@ -26,30 +26,19 @@ public class EngToAzePanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-
-        jLabel1.setText("eng to aze");
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(164, 164, 164)
-                .addComponent(jLabel1)
-                .addContainerGap(176, Short.MAX_VALUE))
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(108, 108, 108)
-                .addComponent(jLabel1)
-                .addContainerGap(176, Short.MAX_VALUE))
+            .addGap(0, 300, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
